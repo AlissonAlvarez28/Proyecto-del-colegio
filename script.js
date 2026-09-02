fa0296553a265a60d5a25b60637b086a8e243162
DayaniLopez <dayanijuriethlopezmartinez@gmail.com>
2026-09-02 09:59:44 -0600

    Agregar botón volver arriba y Script
    Actualización de código para incorporar el botón flotante en la interfaz

--- script.js
+++ script.js
@@ -43,13 +43,14 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
-window.addEventListener('scroll', function() {
-    const btnVolverArriba = document.querySelector('.btn-volver-arriba');
-    if (btnVolverArriba) {
-        if (window.scrollY > 0) { // Cambiado a 0 para que aparezca casi de inmediato
-            btnVolverArriba.classList.add('show');
-        } else {
-            btnVolverArriba.classList.remove('show');
-        }
+
+// Hacer que al hacer clic suba suavemente al inicio
+document.addEventListener('click', function(e) {
+    if (e.target.closest('.btn-volver-arriba')) {
+        e.preventDefault();
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
     }
 });
